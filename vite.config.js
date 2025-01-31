67cfc0e6b54b5894c75f04428f1178e9b4906425
luis <luisnavarrodelangel@gmail.com>
2025-01-31 14:56:32 -0500

    updated to deploy in two different repos

--- vite.config.js
+++ vite.config.js
@@ -5,15 +5,14 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
+const repo = process.env.REPO || 'sari-sari-website'; // Default to your current repo
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  base: '/sari-sari-website/',
+  plugins: [vue(), vueDevTools()],
+  base: `/${repo}/`, // Sets the correct base path dynamically
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-})
+});

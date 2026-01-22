8b5c83f262ed3be4e2935c8327e941278987d93f
sarisarixchange <140453667+sarisarixchange@users.noreply.github.com>
2026-01-22 15:53:32 -0500

    Delete client directory

--- client/src/stores/searchStore.js
+++
@@ -1,9 +0,0 @@
-// stores/searchStore.js
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
-
-export const useSearchStore = defineStore('search', () => {
-  const searchQuery = ref('');
-
-  return { searchQuery };
-});
